cff5391c15ec209ed04ec26de84054bfab476a28
angelineaurel <angelineaurel03@gmail.commkdir>
2025-11-19 15:24:53 +0000

    Add bar plot to show species count for different worlds

--- analysis.R
+++ analysis.R
@@ -23,3 +23,12 @@ starwars %>% slice_max(mass)
 starwars %>% ggplot(aes(x = mass, fill = gender)) +
   geom_density(alpha = 0.8) +
   scale_x_log10()
+
+# A bar plot which shows the number of different species from each home world
+starwars %>%
+  ggplot(aes(y = homeworld, fill = species)) +
+  geom_bar()
+
+
+
+
